0941db9f9839634d9a45b28f227162b1cf214d3f
jarggpt <fajaralfrzi@gmail.com>
2022-02-17 08:07:44 +0000

    new dotriangle

--- dot.c
+++ dot.c
@@ -7,14 +7,20 @@ int main(){
   system("clear");
   printf("how long? ");
   scanf("%i", &l);
-  for(int i=0;i<l;i++){
+  for(i=0;i<l;i++){
+    dot(i);
+  if(i==(l/2)+1){
+    break;
+  }
+  }
+  for(i=l/2;i>0;i--){
     dot(i);
   }
   return 0;
 }
 
 void dot(int w){
-  for(int t=0; t<=w;t++){
+  for(int t=1; t<=w;t++){
     printf(" .");
   }
   printf("\n");
